6cb2593059a73fa3d68cd293b10c72c2a5adab3f
Ivan Mokshin <81585996+LuckyOneDev@users.noreply.github.com>
2026-05-12 11:33:19 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
-Invoke-Expression (&starship init powershell)
+Set-Alias -Name ls -Value eza
+Set-Alias -Name cat -Value bat
+Set-Alias -Name grep -Value rg
+
+oh-my-posh init pwsh --config ~/darkblood.omp.json | Invoke-Expression
 Set-PSReadLineOption -PredictionViewStyle ListView
-Set-Location "C:/"
